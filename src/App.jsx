16be3f46affef5f5b0376c0481a621a0eb769a7c
deco1117 <deco111792@gmail.com>
2023-04-17 16:34:51 +0500

    mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './pages/Home'
 import Students from './pages/Students'
 import Pay from './pages/Pay'
 import Login from './pages/Login'
 import Error from './pages/Error'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { context } from './context';
 
 const App = () => {
+
+
+
   return (
     <div>
       
-          <Routes>
-              <Route path='/' element={<Login/>}/>
-              <Route path='/home' element={<Home/>}/>
-              <Route path='/students' element={<Students/>}/>
-              <Route path='/pay' element={<Pay/>}/>
-              <Route path='*' element={<Error/>}/>
-          </Routes>
+          <context.Provider >
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/home' element={<Home/>}/>
+                <Route path='/students' element={<Students/>}/>
+                <Route path='/pay' element={<Pay/>}/>
+                <Route path='*' element={<Error/>}/>
+            </Routes>
+          </context.Provider>
 
     </div>
   );
--- src/companents/Header/index.jsx
+++ src/companents/Header/index.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import Back from '../../assets/images/back.svg'
 import Bell from '../../assets/images/ball.svg'
+import Sun from '../../assets/images/sun.svg'
+import Moon from '../../assets/images/moon.svg'
 import './index.scss'
 
-const index = () => {
+ const index = () => {
     return (
         <>
-             <header>
+            <header>
                 <div className='header'>
                     <img src={Back} className='header__back'></img>
                     <div className='header__search'>
                         <input type="text" class="form-control header__search--form" placeholder="Search" />
-                        <img src={Bell} className='header__search--bell'/>
+                        <img src={Bell} className='header__search--bell' />
+                        
+                        <form action="" className='form border border-radius-5'>
+                            <input type="radio" />
+                            <label htmlFor=""><img className='cursor-pointer' src={Sun} /></label>
+                            <input type="radio" /><label htmlFor=""><img className='cursor-pointer' src={Moon} /></label>
+                        </form>
                     </div>
                 </div>
 
-             </header>
+            </header>
         </>
     );
 };
